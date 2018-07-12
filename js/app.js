4d88b72f899cdac103acff226b65cad6e670d6f7
Brent <brenteliason@gmail.com>
2018-07-11 21:25:02 -0400

    Added move counter

--- js/app.js
+++ js/app.js
@@ -31,81 +31,92 @@ function shuffle(array) {
 
 let matchCount = 0//tracks how many matched pairs have been made
 let showCount = 0;//tracks how many unmatched cards are shown, should never be more than 2 after match checking is implemented
+let moveCount = 0;//tracks how many times the player has turned over cards (counting cardturns rather than match attempts excludes cheating)
 let lastCard;
 let lastCardSymbol = "";
 let gameFrozen = false;
 
 $('.card').on('click', function () {
-  console.log("\n\nClicked a card");
+  //console.log("\n\nClicked a card");
   let clickedCard = $(this);//saves clicked card
   let cardClasses = String(clickedCard.attr('class'));//saves card's classes, revealing status (e.g. match, show)
 
   if (gameFrozen == false) {//CHECKS IF GAME IS NOT FROZEN because cards are still being turned back over
     if (String(cardClasses) == 'card match') {//clicked on match card, end code
-      console.log("Clicked on a card that's already been matched. DISREGARD!");
+      //console.log("Clicked on a card that's already been matched. DISREGARD!");
     }
     else {//NOT A CARD THAT's ALREADY BEEN MATCHED
       console.log("Clicked on a card that HAS NOT BEEN MATCHED!");
+
+      moveCount++;
+      //console.log("Turn #: " + moveCount);
+      const moveCounter = document.querySelector('.moves');
+      moveCounter.textContent = String(moveCount);
+      //console.log(moveCounter.textContent);
+      //document.getElementsByClassName('.moves')[0];
+      //console.log(String(moveCounter));
+
       let cardChild = $(this).children()[0];
       let cardSymbol = cardChild.classList;//saves class list revealing card symbol
 
       if (cardClasses.includes('show')) {//TURNING CARD BACK OVER, BACK SIDE UP
-        console.log("Turning card back over, hiding symbol");
+        //console.log("Turning card back over, hiding symbol");
         clickedCard.attr('class','card');
         showCount--;
       }
       else {//TURNING CARD OVER TO REVEAL SYMBOL
-        console.log("Turning card over, revealing symbol");
+        //console.log("Turning card over, revealing symbol");
         clickedCard.attr('class','card show');
         showCount++;
-        console.log("Card's symbol is: " + cardSymbol);//prints symbol of card just flipped over
+        //console.log("Card's symbol is: " + cardSymbol);//prints symbol of card just flipped over
       }
       //console.log("The most recent card's classes are now: " + cardClasses);
       //console.log("Total number of cards shown equals: " + showCount);//prints how many cards are face up
       cardClasses = String(clickedCard.attr('class'));
 
       if (showCount == 1) {//save pointer to currentcard, save current card symbol
-        console.log("Running code for 1 card turned over");
+        //console.log("Running code for 1 card turned over");
         lastCard = clickedCard;
         lastCardSymbol = cardSymbol;
-        console.log("Saved pointers, keeping card face up");
+        //console.log("Saved pointers, keeping card face up");
       }
 
       if (showCount == 2) {//flip them both back over, no match checking yet
-        console.log("Running card for two cards turned over");
+        //console.log("Running card for two cards turned over");
         if (String(cardSymbol) == String(lastCardSymbol)) {//Match
           console.log("WE HAVE A MATCH!");
           clickedCard.attr('class','card match');
           lastCard.attr('class','card match');
           matchCount++;
-          console.log("YOU HAVE " + matchCount + " TOTAL MATCHES");
+          //console.log("YOU HAVE " + matchCount + " TOTAL MATCHES");
           showCount = 0;
         }
         else {//CODE FOR NON-MATCH
           console.log("NOT A MATCH!");
-          console.log("Adding delay before turning cards back over");
+          //console.log("Adding delay before turning cards back over");
           console.log("FREEZING GAME");
           gameFrozen = true;
           setTimeout(function () {//DELAYS turning cards back over
             clickedCard.attr('class','card');//turns second card back over
             showCount--;//reduces count after turning second card back over
-            console.log("Turned second card back over because 2 were showing");
+            //console.log("Turned second card back over because 2 were showing");
           }, 1000);
           setTimeout(function () {
             lastCard.attr('class','card');
             showCount--;//reduces count after turning first card back over
-            console.log("Turned first card back over because it was still showing");
+            //console.log("Turned first card back over because it was still showing");
             gameFrozen = false;
             console.log("Game UNFROZEN");
           }, 1200);//IF I CAN FIGURE OUT A WAY TO FREEZE OTHER CLICKS, THEN ADD MORE TIME HERE
           //gameFrozen = false;
         }
       }
-      console.log("Total number of matches shown equals: " + matchCount);
-      console.log("Total number of cards shown (excluding matches) now equals: " + showCount)
+      //console.log("Total number of matches shown equals: " + matchCount);
+      //console.log("Total number of cards shown (excluding matches) now equals: " + showCount)
 
       if (matchCount == 8) {//LAUNCH WINNING CODE
         console.log("CONGRATULATIONS! YOU WON!");
+        console.log("You won in " + moveCount + " moves");
       }
     }
   }
