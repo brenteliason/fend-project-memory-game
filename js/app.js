719e9b6df771875b0b740fee79bdf84289d0f092
Brent <brenteliason@gmail.com>
2018-07-19 21:42:37 -0400

    Cleaned up code, deleted old code that had been commented out

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /*
- * Create a list that holds all of your cards
+ * Create a list that holds all of your cards - used provided code for deck
  */
 
 
@@ -47,29 +47,16 @@ let starRating = 3;
 let lastCard;
 let lastCardSymbol = "";
 let gameFrozen = false;
-//let gameInSession = false;
 let startTime, currentTime, endTime;
 var timerFunction;
 
 function startGame() {
-    //console.log("Inside startGame function");
-    //gameInSession = true;
-    startTimer();
+  startTime = performance.now();
+  timerFunction = setInterval(updateTimer,1000);
     //updateTimer();
     /*while (gameInSession == true) {
       setTimeout(updateTimer(),10000);
     }*/
-}
-
-function startTimer() {
-  //console.log("Inside startTimer function");
-  startTime = performance.now();
-  timerFunction = setInterval(updateTimer,1000);
-  /*gameInSession = true;
-  while (gameInSession == true) {
-    setTimeout(updateTimer(),5000);
-
-  }*/
 }
 
 function updateTimer() {
@@ -94,14 +81,9 @@ function convertTime(milliseconds) {
 
 function endGame() {
   //console.log("Inside endGame function");
-  //gameInSession = false;
   clearInterval(timerFunction);
   endTime = convertTime(currentTime);
-  //stopTimer();
 
-  //const gameTime = endTime - startTime;
-  //const min = Math.floor((gameTime/1000/60) << 0);
-  //const sec = Math.floor((gameTime/1000) % 60);
   //console.log("CONGRATULATIONS! YOU WON!");
   //console.log("You won in " + moveCount + " moves earning a star rating of " + starRating);
   //console.log("Your end time was: " + endTime);
@@ -114,40 +96,13 @@ function endGame() {
   //console.log("You won in " + min + " minutes and " + sec + " seconds");//(endTime - startTime));*/
 }
 
-/*function stopTimer() {
-  console.log("Inside stopTimer function");
-  //clearInterval(updateTimer);
-  endTime = convertTime(performance.now() - startTime);
-  console.log("STOP TIME on timer will be: " + endTime);
-  const timer = document.querySelector('.timer');
-  timer.textContent = endTime;
-}*/
-
 $('.card').on('click', function () {
   //console.log("\n\nClicked a card");
   let clickedCard = $(this);//saves clicked card
   let cardClasses = String(clickedCard.attr('class'));//saves card's classes, revealing status (e.g. match, show)
-  //updateTimer();
 
   if (moveCount == 0) {//start countdown, add to page
     startGame();
-    /*console.log("Starting timer");
-    startTime = performance.now();
-
-    const timer = document.createDocumentFragment();
-    let timerDiv = document.createElement('div');
-    timerDiv.innerText = " " + (performance.now() - startTime) + " ";
-    console.log(timerDiv.innerText);
-    timer.appendChild(timerDiv);
-
-    let restartDiv = document.querySelector('.restart');//grabs restart button to add timer BEFORE
-    restartDiv.insertAdjacentHTML('beforebegin', timer);
-
-    document.body.appendChild(timer);*/
-
-    /*const gameTime = endTime - startTime;
-    const min = Math.floor((gameTime/1000/60) << 0);
-    const sec = Math.floor((gameTime/1000) % 60);*/
   }
 
   if (gameFrozen == false) {//CHECKS IF GAME IS NOT FROZEN because cards are still being turned back over
@@ -156,7 +111,6 @@ $('.card').on('click', function () {
     }
     else {//NOT A CARD THAT's ALREADY BEEN MATCHED
       //console.log("Clicked on a card that HAS NOT BEEN MATCHED!");
-      //updateTimer();
       moveCount++;
       //console.log("Turn #: " + moveCount);
       const moveCounter = document.querySelector('.moves');
@@ -169,15 +123,6 @@ $('.card').on('click', function () {
         firstStar.remove();
         starRating--;
       }
-    /*  if (moveCount == 40) {//remove second star
-        const rating = document.querySelector('.stars');
-        const firstStar = rating.firstElementChild;
-        firstStar.remove();
-        starRating--;
-      }*/
-      //console.log(moveCounter.textContent);
-      //document.getElementsByClassName('.moves')[0];
-      //console.log(String(moveCounter));
 
       let cardChild = $(this).children()[0];
       let cardSymbol = cardChild.classList;//saves class list revealing card symbol
@@ -231,7 +176,6 @@ $('.card').on('click', function () {
             gameFrozen = false;
             //console.log("Game UNFROZEN");
           }, 1200);//IF I CAN FIGURE OUT A WAY TO FREEZE OTHER CLICKS, THEN ADD MORE TIME HERE
-          //gameFrozen = false;
         }
       }
       //console.log("Total number of matches shown equals: " + matchCount);
@@ -240,67 +184,18 @@ $('.card').on('click', function () {
       if (matchCount == 8) {//LAUNCH WINNING CODE
         endGame();
         //console.log("Stopping timer");
-        //endTime = performance.now();
-        /*const gameTime = endTime - startTime;
-        const min = Math.floor((gameTime/1000/60) << 0);
-        const sec = Math.floor((gameTime/1000) % 60);
-        console.log("CONGRATULATIONS! YOU WON!");
-        console.log("You won in " + moveCount + " moves earning a star rating of " + starRating);
-        console.log("You won in " + min + " minutes and " + sec + " seconds");//(endTime - startTime));*/
       }
     }
-  }
-  else {//GAME FROZEN while cards turn back over
-    //console.log("CARDS haven't been turned back over yet, GAME FROZEN");
-  }
+  }//END OF IF STATEMENT ensuring cards are only turned over when game isn't frozen from cards being turned back over after a failed match attempt
 
-
-//check for match, if showcount equals 1, save currentCardSymbol as lastCardSymbol, if showcount equals 2, check if current cardsymbol equals last cardsymbol
-
-  /*if (showCount == 1) {
-    lastCard = clickedCard;
-    lastCardSymbol = cardSymbol;
-    console.log("We need another " + lastCardSymbol + " for a match");
-  }
-  else if (showCount == 2) {
-    console.log("The last card was a: " + lastCardSymbol + ". The new card is a: " + cardSymbol)
-    if (String(lastCardSymbol) == String(cardSymbol)) {
-      console.log("We have a match!");
-      clickedCard.attr('class','card match');
-      lastCard.attr('class','card match');
-    }
-    else {
-      console.log("1. No match! Turn cards back over! Try again!");
-    }
-  }
-  else {
-    console.log("2. No match! Turn cards back over! Try again!");
-    showCount = 0;
-    clickedCard.attr('class','card');
-    lastCard.attr('class','card');
-  }*/
-
-  //console.log($(this));
-
-
-
-  /*if (showCount == 1)
-    lastCardSymbol = cardSymbol;//saves cardSymbol
-  else {
-    if (lastCardSymbol == cardSymbol) {
-      console.log("We have a match!")
-    }
-  }*/
-
-})
+})//END of function for clicking on a card
 
 $('.restart').on('click', function () {
   //console.log("Inside restart function");
   gameFrozen = true;//freeze game board during restart process
 
   //delete any congratulations message if game WON
-  if (matchCount == 8)
-  {
+  if (matchCount == 8) {
     //console.log("Need to remove congratulations message");
     const messageToDelete = document.querySelector('span');//selects first span which is the congrats message
     messageToDelete.remove();
@@ -309,7 +204,6 @@ $('.restart').on('click', function () {
     clearInterval(timerFunction);//stops timer
   }
 
-
   //Need to turn all the cards back OVER
   const allCards = document.querySelectorAll('.card');
   //console.log("All of the cards in the deck: " + allCards);
@@ -317,7 +211,7 @@ $('.restart').on('click', function () {
     card.classList.remove('match','show');
   })
 
-  //restart matchCount and showCount variables, point card pointers to null
+  //restart matchCount and showCount variables, point card pointers to null so there's no "match" between games
   matchCount = 0;
   showCount = 0;
   lastCard = null;
@@ -328,20 +222,18 @@ $('.restart').on('click', function () {
   while (starRating < 3) {
     //console.log("Need to add more stars");
     const rating = document.querySelector('.stars');
-    //Need to add "<li><i class="fa fa-star"></i></li>" back in
+    //Need to add "<li><i class="fa fa-star"></i></li>" back in for each star that was lost in previous game
     const starHTML = '<li><i class="fa fa-star"></i></li>';
     rating.insertAdjacentHTML('beforeend',starHTML);
-    //const star = rating.firstElementChild;
-    //star.clone().appendTo(rating);
     starRating++;
   }
 
-  //Need to restart movecount variable and on page
+  //Need to restart movecount variable in code and on page
   moveCount = 0
   const moveCounter = document.querySelector('.moves');
   moveCounter.textContent = String(moveCount);
 
-  //restart timer variables and on page
+  //restart timer variables in code and on page
   startTime, currentTime, endTime = null;
   const timer = document.querySelector('.timer');
   timer.textContent = "0:00";
@@ -351,7 +243,6 @@ $('.restart').on('click', function () {
   //console.log("Need to shuffle cards");
   let cardDeck = Array.from(document.querySelectorAll('.card'));
   const deckHTML = document.querySelector('.deck');
-  //deckHTML.innerHTML = "";
   cardDeck.forEach(function(oldCard) {
     //console.log("Removing: " + String(oldCard));
     deckHTML.removeChild(oldCard);
@@ -364,15 +255,10 @@ $('.restart').on('click', function () {
     deckHTML.appendChild(newCard);
   })
 
-/*console.log("Testing shuffle function with normal array: ");
-var arr = [2, 11, 37, 42];
-arr = shuffle(arr);
-console.log(arr);*/
-
-
-  /*console.log("All of the cards in the deck: " + allCards);
-  allCards.forEach(function(card) {
-    card.classList.remove('match','show');*/
+  /*console.log("Testing shuffle function with normal array: ");
+  var arr = [2, 11, 37, 42];
+  arr = shuffle(arr);
+  console.log(arr);*/
 
   gameFrozen = false;
-})
+})//END of function for clicking restart button
